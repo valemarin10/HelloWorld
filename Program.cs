cec1c7581b64f0a0a1df3d82db948e4fbd21d49d
Paula Marin <pvm@mail.usf.edu>
2020-01-23 00:16:55 -0500

    Version 2 HelloWorld

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //pause to display hello world!
+            Console.WriteLine("Press any key to continue...");
+
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey(true);
         }
     }
 }
